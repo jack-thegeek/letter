fefb582f71402fe06313fa060c8e7331ce7525b4
Jack-thegeek <jackson52076@gmail.com>
2022-06-27 23:21:41 +0800

    -webkit-tap-highlight-color

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,43 +1,51 @@
 <script setup>
-    import {ref} from 'vue'
+import {ref} from 'vue'
 
-    let pop = ref(false)
+let pop = ref(false)
 </script>
 
 <template>
-    <div>
-        <var-app-bar title="title" title-position="center">
-            <template #left>
-                <var-button
-                        round
-                        text
-                        color="transparent"
-                        text-color="#fff"
-                        @click="pop=true">
-                    <var-icon name="menu" :size="24"/>
-                </var-button>
-            </template>
-        </var-app-bar>
-        <var-popup v-model:show="pop" position="left">
-            <div class="menu" @click="pop=false">
-                <router-link to="/">Home</router-link>
-                <router-link to="/about">About</router-link>
-                <router-link to="/album">Album</router-link>
-            </div>
-        </var-popup>
-    </div>
+	<div>
+		<var-app-bar title="title" title-position="center">
+			<template #left>
+				<var-button
+						round
+						text
+						color="transparent"
+						text-color="#fff"
+						@click="pop=true">
+					<var-icon name="menu" :size="24"/>
+				</var-button>
+			</template>
+		</var-app-bar>
+		<var-popup v-model:show="pop" position="left">
+			<div class="menu" @click="pop=false">
+				<router-link to="/" v-ripple="{ color: '#ccc' }">Home</router-link>
+				<router-link to="/about" v-ripple="{ color: '#ccc' }">About</router-link>
+				<router-link to="/album" v-ripple="{ color: '#ccc' }">Album</router-link>
+			</div>
+		</var-popup>
+	</div>
 </template>
 
 <style lang="scss">
-    .menu {
-        padding: 20px 0;
-        width: 250px;
+	.menu {
+		padding: 20px 0;
+		width: 250px;
 
-        a {
-            display: block;
-            text-decoration: none;
-            padding: 10px 25px;
-        }
+		a {
+			display: block;
+			padding: 10px 25px;
+		}
 
-    }
+		a:link, a:visited, a:hover {
+			color: black;
+			background-color: whitesmoke;
+		}
+
+		a:active {
+			color: #2c3e50;
+			background-color: whitesmoke;
+		}
+	}
 </style>
