bb5e5ecbceba7605b59a1e268c473a5b368e936a
Jack-thegeek <jackson52076@gmail.com>
2022-06-29 21:12:48 +0800

    peerjs

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
-    <div>
-        About
-    </div>
+	<div>
+		About
+		<p>{{peerId}}</p>
+	</div>
 </template>
+<script>
+import {ref} from 'vue'
+import Peer from 'peerjs'
+
+export default {
+    data() {
+        return {
+            peerId: ''
+        }
+    },
+    mounted: function () {
+        this.init()
+    },
+    methods: {
+        init() {
+	        this.peerId = 'getting id ...'
+            const peer = new Peer()
+            peer.on('open', (id)=>{
+                console.log('My peer ID is: ' + id)
+                this.peerId = 'My peer ID is: ' + id
+            })
+        }
+    }
+}
+</script>
