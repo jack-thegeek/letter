78b5a4347dc2bf3fd732d4f96226e2245b2c61c3
Jack-thegeek <jackson52076@gmail.com>
2022-08-03 00:16:45 +0800

    album

--- src/views/Album.vue
+++ src/views/Album.vue
@@ -1,11 +1,33 @@
 <script setup>
 import axios from '@/request'
-import { reactive } from 'vue'
-import { Lazy, Virtual } from 'swiper'
+import { ref, reactive } from 'vue'
 import 'swiper/css'
 import 'swiper/css/lazy'
+import 'swiper/css/pagination'
+import { Lazy, Pagination, Virtual } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import dayjs from 'dayjs'
 
-const modules = [Virtual, Lazy]
+const lazyOpts = {
+    loadPrevNext: true,
+    loadOnTransitionStart: true
+}
+
+const paginationOpts = {
+    type: 'fraction'
+}
+
+const modules = [Virtual, Lazy, Pagination]
+
+let swiperRef = null
+const onSwiper = (swiper) => {
+    swiperRef = swiper
+}
+const page = ref('')
+const slideTo = (index) => {
+    swiperRef.slideTo(index - 1, 0)
+}
+
 let state = reactive({
     images: []
 })
@@ -13,6 +35,9 @@ let state = reactive({
 const getImages = async () => {
     const result = await axios.get('/album')
     if (result.data) {
+        result.data.forEach(value => {
+            value.date = dayjs(value.date).format('YYYY-MM-DD')
+        })
         state.images = result.data
     }
 }
@@ -20,35 +45,78 @@ const getImages = async () => {
 getImages()
 </script>
 <template>
-	<div>
-		<swiper :modules="modules" :slides-per-view="1" lazy virtual>
-			<swiper-slide
-					:key="index"
-					:virtualIndex="index"
-					v-for="(image, index) in state.images">
-				<img alt="" class="swiper-lazy pic" v-bind:data-src="image.src">
-				<div class="description">
-					{{image.title}}
+	<div class="full-width">
+		<div class="jump">
+			<var-input placeholder="跳转到" v-model="page"/>
+			<var-button type="primary" round @click="slideTo(page)">
+				<span class="material-icons">
+					navigate_next
+				</span>
+			</var-button>
+		</div>
+		<swiper :pagination="paginationOpts" :lazy="lazyOpts" :modules="modules" :slides-per-view="1"
+		        @swiper="onSwiper" virtual>
+			<swiper-slide :key="index" :virtualIndex="index" v-for="(image, index) in state.images">
+				<div class="img-box">
+					<img :data-src="image.url" alt="" class="swiper-lazy pic">
 				</div>
-				<div class="description">
-					{{image.date}}
-				</div>
-				<div class="description">
-					{{image.content}}
+				<div class="info">
+					<div>{{ image.title }}</div>
+					<div class="content">{{ image.content }}</div>
+					<div class="date">{{ image.date }}</div>
 				</div>
 				<div class="swiper-lazy-preloader"></div>
 			</swiper-slide>
 		</swiper>
+
+
 	</div>
 </template>
-<style lang="scss">
+<style lang="scss" scoped>
+	.img-box {
+		width: 100vw;
+		min-height: 0.75vw;
+	}
 	.pic {
 		width: 100%;
-		height: 150px;
-		object-fit: cover;
+		height: auto;
+		object-fit: fill;
 	}
 
-	.description {
+	.jump {
+		padding: 5px 20px;
+		margin-bottom: 20px;
+		display: flex;
+		justify-content: start;
+		align-items: flex-end;
+
+		.page-label {
+			padding: 5px 20px 5px 5px;
+		}
+	}
+
+	.var-button--round {
+		height: var(--button-normal-height);
+		margin-left: 10px;
+	}
+
+	.var-input {
+		width: 80%;
+	}
+
+	.info {
+		background: white;
+		padding: 16px;
+
+		.content {
+			line-height: 1.8;
+		}
+
+		.date {
+			color: #ddd;
+			text-align: right;
+			margin-top: 15px;
+		}
 
 	}
 </style>
